323a84baf4e1c278463c1fce924a70e2f847a24c
yamaday0u <35hallelujah00@gmail.com>
2021-02-28 00:03:52 +0900

    defined method "after_sign_in_path_for(resource)"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :identity])
   end
+
+  def after_sign_in_path_for(resource)
+    calendars_path
+  end
+
 end

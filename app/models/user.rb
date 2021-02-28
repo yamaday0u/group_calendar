35c596e4d76cea79363511835f034f21bbf397c7
yamaday0u <35hallelujah00@gmail.com>
2021-02-28 16:37:36 +0900

    Added validation for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  with_options presence: true do
+    validates :name
+
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'must include at least one half-width English character and a number', on: :create # validate only users/registrations#create
+
+  end
+  
   
 end

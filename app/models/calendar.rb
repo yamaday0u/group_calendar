75d1c8d89d42bf21cf09415f4cd5fe33971f8544
yamaday0u <35hallelujah00@gmail.com>
2021-03-06 14:39:20 +0900

    Defined associations for tables between users & calendars

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,2 +1,4 @@
 class Calendar < ApplicationRecord
+  has_many :user_calendars
+  has_many :users, through: :user_calendars
 end
--- app/models/user_calendar.rb
+++ app/models/user_calendar.rb
@@ -1,2 +1,4 @@
 class UserCalendar < ApplicationRecord
+  belongs_to :user
+  belongs_to :calendar
 end

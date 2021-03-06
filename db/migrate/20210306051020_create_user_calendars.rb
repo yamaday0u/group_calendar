967e05bc8e446e5eb1ff8eb9593fc3d2c43853e4
yamaday0u <35hallelujah00@gmail.com>
2021-03-06 14:37:39 +0900

    rails db:migrate for intermediate tables "user_calendars"

--- db/migrate/20210306051020_create_user_calendars.rb
+++ db/migrate/20210306051020_create_user_calendars.rb
@@ -1,7 +1,8 @@
 class CreateUserCalendars < ActiveRecord::Migration[6.0]
   def change
     create_table :user_calendars do |t|
-
+      t.references :user, foreign_key: true
+      t.references :calendar, foreign_key: true
       t.timestamps
     end
   end

615a7e3075429644c61a234a506ee2e140ab3a15
yamaday0u <35hallelujah00@gmail.com>
2021-03-06 14:45:18 +0900

    Updated route for calendars#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  resources :calendars, only: [:index, :new, :create]
+  resources :calendars, only: [:index, :new, :create, :show]
   devise_for :users
 end

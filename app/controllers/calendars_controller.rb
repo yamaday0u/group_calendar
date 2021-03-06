e479a22eb80e0f2a8a93c04fcfdd81b6be1b11da
yamaday0u <35hallelujah00@gmail.com>
2021-03-06 13:56:29 +0900

    Defined action for calendars#create

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -6,4 +6,23 @@ class CalendarsController < ApplicationController
     @calendar = Calendar.new
   end
 
+  def create
+    @calendar = Calendar.new(calendar_params)
+    if @calendar.save
+      redirect_to calendars_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def calendar_params
+    params.require(:calendar).permit(
+      :title,
+      :content,
+      :start_time,
+      :end_time
+    )
+  end
+
 end

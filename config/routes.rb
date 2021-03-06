bf61b9bf6c474a62736d16664cee29281e23fcb2
yamaday0u <35hallelujah00@gmail.com>
2021-03-06 17:04:52 +0900

    Defined route for calendars#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  resources :calendars, only: [:index, :new, :create, :show]
+  resources :calendars, only: [:index, :new, :create, :show, :destroy]
   devise_for :users
 end

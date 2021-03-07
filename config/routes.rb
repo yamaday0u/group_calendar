a312a0bdb2d9944599bf6a0856dda58bcb6c0bf3
yamaday0u <35hallelujah00@gmail.com>
2021-03-07 23:29:45 +0900

    Defined route for groups#new & create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'groups/index'
   root to: 'home#index'
   resources :calendars
-  resources :groups, only: :index
+  resources :groups, only: [:index, :new, :create]
   devise_for :users
 end

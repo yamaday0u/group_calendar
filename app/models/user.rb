6bcebdd9845f701f64f7ba19a2d381ed2b9ab0f2
yamaday0u <35hallelujah00@gmail.com>
2021-03-08 20:22:04 +0900

    Defined association between users & groups model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :user_groups
+  has_many :groups, through: :user_groups
+
   has_many :user_calendars
   has_many :calendars, through: :user_calendars
 

73921767012e001badb9d306758fdf1714d88859
yamaday0u <35hallelujah00@gmail.com>
2021-03-10 00:12:59 +0900

    Defined route for users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'groups/index'
   root to: 'home#index'
+  resources :users, only: :index
   resources :calendars
   resources :groups, only: [:index, :new, :create, :show]
-  devise_for :users
+  deevise_for :users
 end

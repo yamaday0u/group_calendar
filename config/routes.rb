250a8a63cf7450c9cfcea60ca8fde948864c3dd8
yamaday0u <35hallelujah00@gmail.com>
2021-03-10 01:20:19 +0900

    Added page & link for "coming soon"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'groups/index'
   root to: 'home#index'
+  get 'home/new'
   resources :users, only: :index
   resources :calendars
   resources :groups, only: [:index, :new, :create, :show]

520a91d1264c9d7b73f5177a4ef80aa6fdc11ca8
yamaday0u <35hallelujah00@gmail.com>
2021-03-09 23:57:50 +0900

    Fixed form for groups action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @groups = Group.all
   end
@@ -8,6 +10,22 @@ class GroupsController < ApplicationController
   end
 
   def create
-    
+    @group = Group.new(group_params)
+    if @group.valid?
+      @group.save
+      redirect_to groups_path, notice: 'Created new group'
+    else
+      render 'new'
+    end
+  end
+
+  private
+  
+  def group_params
+    params.require(:group).permit(
+      :name,
+      :outline,
+      :task
+    )
   end
 end

e5205ad9cfe6052ebaea9d25ab2ccc20f74f3886
yamaday0u <35hallelujah00@gmail.com>
2021-03-11 00:40:56 +0900

    Defined redirect destination after update user's information

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,6 +40,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # protected
 
+  def after_update_path_for(resource)
+    user_path(current_user)
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])

99703ecc1bef4c0a558880be2fbcaaf2da60230a
yamaday0u <35hallelujah00@gmail.com>
2021-03-11 00:07:08 +0900

    Switched route order registrations "users/registrations" & resources :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   get 'home/new'
-  resources :users, only: [:index, :show]
   resources :calendars
   resources :groups, only: [:index, :new, :create, :show]
   devise_for :users, controllers: {
     registrations: "users/registrations"
   }
+  resources :users, only: [:index, :show]
 end

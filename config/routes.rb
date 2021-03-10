6b5186883a8ee28f46e8f7208e6fd224d35cc9a1
yamaday0u <35hallelujah00@gmail.com>
2021-03-10 23:12:54 +0900

    Defined route for users#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   get 'home/new'
-  resources :users, only: :index
+  resources :users, only: [:index, :show]
   resources :calendars
   resources :groups, only: [:index, :new, :create, :show]
   devise_for :users

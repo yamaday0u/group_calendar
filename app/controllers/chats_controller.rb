1c417ebd224df20a996badfd29a1b714c3568c3f
yamaday0u <35hallelujah00@gmail.com>
2021-03-14 23:10:21 +0900

    Updated action for chats#index & create

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,6 @@
 class ChatsController < ApplicationController
   def index
-    @chats = Chat.includes(:group).where(group_id: params[:group_id])
+    @chats = Chat.includes(:group).where(group_id: params[:group_id]).order(created_at: :desc).limit(10)
     @chat = Chat.new
   end
 
@@ -8,7 +8,8 @@ class ChatsController < ApplicationController
     @chat = Chat.new(chat_params)
     if @chat.save
       @user = current_user
-      ActionCable.server.broadcast 'chat_channel', content: @chat, user: @user
+      @time = @chat.created_at.strftime("%Y-%m-%-d %-H:%-M")
+      ActionCable.server.broadcast 'chat_channel', content: @chat, user: @user, time: @time
     end
   end
 

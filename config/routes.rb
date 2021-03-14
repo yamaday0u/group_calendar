0c30c33d9d9dd2988d083573ea8594109c37295a
yamaday0u <35hallelujah00@gmail.com>
2021-03-14 21:26:11 +0900

    Defined route for chats#index & create

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get 'home/new'
   resources :calendars
-  resources :groups, only: [:index, :new, :create, :show]
+  resources :groups, only: [:index, :new, :create, :show] do
+    resources :chats, only: [:index, :create]
+  end
   devise_for :users, controllers: {
     registrations: "users/registrations"
   }

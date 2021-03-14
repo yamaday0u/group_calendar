782fe5ff8f88c9221d806a0d5f3dc7a75c3920de
yamaday0u <35hallelujah00@gmail.com>
2021-03-14 21:41:54 +0900

    Defined action for chats#create & Updated action for chats#index

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,22 @@
 class ChatsController < ApplicationController
   def index
     @chats = Chat.includes(:group).where(group_id: params[:id])
+    @chat = Chat.new
+  end
+
+  def create
+    @chat = Chat.new(chat_params)
+    if @chat.save
+      @user = current_user
+      ActionCable.server.broadcast 'chat_channel', content: @chat, user: @user
+    end
+  end
+
+  private
+  def chat_params
+    params.require(:chat).permit(:text).merge(
+      user_id: current_user.id,
+      group_id: params[:group_id]
+    )
   end
 end

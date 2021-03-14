e497add7104e240213883a32c892613be914d6d4
yamaday0u <35hallelujah00@gmail.com>
2021-03-14 21:16:00 +0900

    rails d model chat

--- db/migrate/20210314120508_create_chats.rb
+++
@@ -1,8 +0,0 @@
-class CreateChats < ActiveRecord::Migration[6.0]
-  def change
-    create_table :chats do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_140926) do
+ActiveRecord::Schema.define(version: 2021_03_14_121413) do
 
   create_table "calendars", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false

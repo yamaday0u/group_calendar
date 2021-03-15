9ea35a9c98eec6094d3bbe858058211bead981c3
yamaday0u <35hallelujah00@gmail.com>
2021-03-15 21:31:27 +0900

    Defined action for calendar_for_groups#create

--- app/controllers/calendar_for_groups_controller.rb
+++ app/controllers/calendar_for_groups_controller.rb
@@ -9,4 +9,28 @@ class CalendarForGroupsController < ApplicationController
   def new
     @g_calendar = CalendarForGroup.new
   end
+
+  def create
+    @g_calendar = CalendarForGroup.new(g_calendar_params)
+    if @g_calendar.valid?
+      @g_calendar.save
+      redirect_to group_calendar_for_groups_path(@g_calendar.group_id), notice: 'Added schejule'
+    else
+      render 'new'
+    end    
+  end
+
+  private
+
+  def g_calendar_params
+    params.require(:calendar_for_group).permit(
+      :title,
+      :content,
+      :start_time,
+      :end_time
+    ).merge(
+      user_id: current_user.id,
+      group_id: params[:group_id]
+    )
+  end
 end

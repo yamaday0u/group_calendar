db84a6585b2e2587a68cbe01d021fbd1647983ec
yamaday0u <35hallelujah00@gmail.com>
2021-03-15 21:54:45 +0900

    Added link to calendar_for_groups#show

--- app/controllers/calendar_for_groups_controller.rb
+++ app/controllers/calendar_for_groups_controller.rb
@@ -2,7 +2,7 @@ class CalendarForGroupsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @g_calendars = CalendarForGroup.where(group_id: params[:group_id])
+    @g_calendars = CalendarForGroup.includes(:group).where(group_id: params[:group_id])
     @group = Group.find(params[:group_id])
   end
 

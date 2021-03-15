2cd042af30eacf9f871457329f09d2344f59841d
yamaday0u <35hallelujah00@gmail.com>
2021-03-15 21:10:29 +0900

    Defined action for group_calendar_for_groups#new

--- app/controllers/calendar_for_groups_controller.rb
+++ app/controllers/calendar_for_groups_controller.rb
@@ -1,6 +1,12 @@
 class CalendarForGroupsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @g_calendars = CalendarForGroup.where(group_id: params[:group_id])
     @group = Group.find(params[:group_id])
+  end
+
+  def new
+    @g_calendar = CalendarForGroup.new
   end
 end

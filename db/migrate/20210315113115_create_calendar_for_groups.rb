3cafcf84592e84cff5c4ab6aa97e15dc089dc91c
yamaday0u <35hallelujah00@gmail.com>
2021-03-15 20:37:44 +0900

    Created calendar_for_groups table

--- db/migrate/20210315113115_create_calendar_for_groups.rb
+++ db/migrate/20210315113115_create_calendar_for_groups.rb
@@ -1,7 +1,12 @@
 class CreateCalendarForGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :calendar_for_groups do |t|
-
+      t.string :title, null: false
+      t.string :content, null: false
+      t.datetime :start_time, null: false
+      t.datetime :end_time, null: false
+      t.references :user, null: false, foreign_key: true
+      t.references :group, null: false, foreign_key: true
       t.timestamps
     end
   end

7cacb535e8c91ad3c7ff92eabea1bf4460d6f9a2
yamaday0u <35hallelujah00@gmail.com>
2021-03-15 21:06:09 +0900

    Updated for display group name temporarily

--- app/controllers/calendar_for_groups_controller.rb
+++ app/controllers/calendar_for_groups_controller.rb
@@ -1,5 +1,6 @@
 class CalendarForGroupsController < ApplicationController
   def index
-    @g_calendars = CalendarForGroup.where(group_id: params[:id])
+    @g_calendars = CalendarForGroup.where(group_id: params[:group_id])
+    @group = Group.find(params[:group_id])
   end
 end

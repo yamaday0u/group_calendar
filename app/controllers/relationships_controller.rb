29376b40744097558a9ab2f80385ab941fec8c3d
yamaday0u <35hallelujah00@gmail.com>
2021-03-17 23:39:16 +0900

    Defined action for relationships#destroy

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,4 +3,10 @@ class RelationshipsController < ApplicationController
     Relationship.create(follower_id: current_user.id, followed_id: params[:user_id])
     redirect_to user_path(params[:user_id]), notice: "Requested successfully!"
   end
+
+  def destroy
+    other_user = User.find(params[:user_id])
+    current_user.unfollow(other_user)
+    redirect_to user_path(params[:user_id]), notice: "Canceld request"
+  end
 end

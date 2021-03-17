6409226cbdd2e2244191041afda470718236bdcf
yamaday0u <35hallelujah00@gmail.com>
2021-03-17 23:00:16 +0900

    Defined action for relationships#create

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,6 @@
 class RelationshipsController < ApplicationController
+  def create
+    Relationship.create(follower_id: current_user.id, followed_id: params[:user_id])
+    redirect_to user_path(params[:user_id]), notice: "Requested successfully!"
+  end
 end

b97eb56cbc8b208ea405b283e2b39934060d2eb7
yamaday0u <35hallelujah00@gmail.com>
2021-03-17 23:31:03 +0900

    Update 20210317121348_create_relationships.rb

--- db/migrate/20210317121348_create_relationships.rb
+++ db/migrate/20210317121348_create_relationships.rb
@@ -1,8 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[6.0]
   def change
     create_table :relationships do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+      t.integer :follower_id # user who is following others (active relationship)
+      t.integer :followed_id # user who is followed by others (passive relationship)
 
       t.timestamps
     end

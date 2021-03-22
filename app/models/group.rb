6355ddcc1e3122f588e5cb959c82d7e598604745
yamaday0u <35hallelujah00@gmail.com>
2021-03-22 21:46:27 +0900

    Implemented unit test for group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,10 @@
 class Group < ApplicationRecord
+  # Associations
   has_many :user_groups
   has_many :users, through: :user_groups
   has_many :calendar_for_groups
   has_many :chats
 
+  # Validation
   validates :name, presence: true
 end

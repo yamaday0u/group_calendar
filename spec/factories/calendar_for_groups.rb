c2d2152bc6c1422eff15b79b886c3e03513d935d
yamaday0u <35hallelujah00@gmail.com>
2021-03-23 21:01:44 +0900

    Implemented unit test for calendar_for_group model

--- spec/factories/calendar_for_groups.rb
+++ spec/factories/calendar_for_groups.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :calendar_for_group do
+    title { Faker::Lorem.word }
+    content { Faker::Lorem.word }
+    start_time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
+    end_time { Faker::Time.between(from: DateTime.now - 1, to: DateTime.now) }
+    association :user
+    association :group
   end
 end
--- spec/models/calendar_for_group_spec.rb
+++ spec/models/calendar_for_group_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe CalendarForGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @group_calendar = FactoryBot.build(:calendar_for_group)
+  end
+
+  describe '#create' do
+    context 'successfully' do
+      it 'is valid with title, content, start_time, end_time' do
+        expect(@group_calendar).to be_valid
+      end
+      it 'is valid without content' do
+        @group_calendar.content = ''
+        expect(@group_calendar).to be_valid
+      end
+    end
+    context 'unsuccessfully' do
+      it 'is invalid without title' do
+        @group_calendar.title = ''
+        @group_calendar.valid?
+        expect(@group_calendar.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'is invalid without user' do
+        @group_calendar.user = nil
+        @group_calendar.valid?
+        expect(@group_calendar.errors.full_messages).to include("User must exist")
+      end
+      it 'is invalid without group' do
+        @group_calendar.group = nil
+        @group_calendar.valid?
+        expect(@group_calendar.errors.full_messages).to include("Group must exist")
+      end
+    end
+  end
 end

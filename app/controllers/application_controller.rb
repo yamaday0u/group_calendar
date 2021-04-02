eac23fcb8e218276640716a406c84e02e659328d
yamaday0u <35hallelujah00@gmail.com>
2021-04-02 22:46:40 +0900

    Add column "user_image" to configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name identity])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name identity])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name identity user_image])
   end
 
   def after_sign_in_path_for(_resource)

e3901218987dbffe5279fdaaee537a17302baaad
yamaday0u <35hallelujah00@gmail.com>
2021-04-06 07:27:31 +0900

    Defined association between user & group_image

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,7 @@ class Group < ApplicationRecord
   has_many :users, through: :user_groups
   has_many :calendar_for_groups
   has_many :chats
+  has_one_attached :group_image
 
   # Validation
   validates :name, presence: true

fb220997440e089bde25b8161cf160ea944b5f99
yamaday0u <35hallelujah00@gmail.com>
2021-04-10 10:18:16 +0900

    Implemented the function for guest user log in

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    post 'users/guest_log_in', to: 'users/sessions#guest_log_in'
+  end
   resources :users, only: %i[index show] do
     resources :relationships, only: %i[create destroy]
   end

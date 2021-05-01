b9c8fe777bca9429af3eefdb26c3bea4cc4560bf
yamaday0u <35hallelujah00@gmail.com>
2021-05-02 02:19:59 +0900

    Updated JavaScript files for apply to specific views

--- app/javascript/set_checked.js
+++ app/javascript/set_checked.js
@@ -1,25 +1,23 @@
-const checkedStatus = () => {
-  // get elements of calendars for month, week & custom 
-  // const monthCalendarElement = document.getElementById("month_calendar");
-  // const weekCalendarElement = document.getElementById("week_calendar");
-  // const customCalendarElement = document.getElementById("custom_calendar");
+if (location.pathname.match("/calendars") || location.pathname.match("/calendar_for_groups")) {
+  const checkedStatus = () => {
 
-  // // get elements of checkboxes for month, week & custom 
-  const monthCalendar = document.getElementById("calendar_month");
-  const weekCalendar = document.getElementById("calendar_week");
-  const customCalendar = document.getElementById("calendar_custom");
-
-  console.log("set")
-
-  if (sessionStorage.key(0)==='month') {
-    console.log(sessionStorage.key(0));
-    monthCalendar.checked = "true";
-  }else if (sessionStorage.key(1)==='week') {
-    console.log(sessionStorage.key(1));
-    weekCalendar.checked = "true";
-  }else if (sessionStorage.key(1)==='custom') {
-    console.log(sessionStorage.key(1));
-    customCalendar.checked = "true";
+    // // get elements of checkboxes for month, week & custom 
+    const monthCalendar = document.getElementById("calendar_month");
+    const weekCalendar = document.getElementById("calendar_week");
+    const customCalendar = document.getElementById("calendar_custom");
+  
+    console.log("set")
+  
+    if (sessionStorage.key(0)==='month') {
+      console.log(sessionStorage.key(0));
+      monthCalendar.checked = "true";
+    }else if (sessionStorage.key(1)==='week') {
+      console.log(sessionStorage.key(1));
+      weekCalendar.checked = "true";
+    }else if (sessionStorage.key(1)==='custom') {
+      console.log(sessionStorage.key(1));
+      customCalendar.checked = "true";
+    }
   }
+  window.addEventListener("load", checkedStatus);
 }
-window.addEventListener("load", checkedStatus);

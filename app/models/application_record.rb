3f11db9b31d3af0e6a776887e71ca16a854eeeaa
yamaday0u <35hallelujah00@gmail.com>
2021-05-04 14:11:41 +0900

    Defined custom method "start_end_check" for validate

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def start_end_check # is used in models/calendar.rb & calendar_for_group.rb
+    errors.add(:end_time, "must set be late than Start time") if self.start_time > self.end_time
+  end
 end

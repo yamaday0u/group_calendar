1f49a29ed95e8b22fcb6c6afff4ca8d202921fbd
yamaday0u <35hallelujah00@gmail.com>
2021-05-17 21:46:46 +0900

    Create index.jsx

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -5,22 +5,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
+import { BrowserRouter } from 'react-router-dom'; //アプリケーションにrouterを導入するためのコンポーネント
+import Room from '../components/Room'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+    <BrowserRouter>
+      <Room/>
+    </BrowserRouter>,
+    document.querySelector('#room-root'),
+  );
+});

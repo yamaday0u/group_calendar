a99521e64764d94f44350e5c85ad536669f450a7
yamaday0u <35hallelujah00@gmail.com>
2021-05-19 21:15:34 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   get 'home/new'
+
+  get 'rooms', to: 'site#index'
+
   resources :calendars
   resources :groups do
     resources :chats, only: %i[index create]

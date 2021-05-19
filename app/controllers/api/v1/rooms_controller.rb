01c356c50c83e476c988168bbbef60fdfaf90b85
yamaday0u <35hallelujah00@gmail.com>
2021-05-19 21:15:39 +0900

    Update rooms_controller.rb

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::RoomsController < ApplicationController
+  protect_from_forgery with: :null_session
   def index
+    mates = current_user.matchers
+    render json: mates
   end
 end

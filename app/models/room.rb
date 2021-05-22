46cfb2224e2bd9fb89fd7921fe76b46ba1a42c88
yamaday0u <35hallelujah00@gmail.com>
2021-05-23 00:24:01 +0900

    Update room.rb

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,8 @@
 class Room < ApplicationRecord
-  has_many :mate_chats 
+  # Associations
+  has_many :mate_chats
+ 
   belongs_to :talker_1, class_name: "User"
   belongs_to :talker_2, class_name: "User"
+
 end

a51184f876f2ef7e52a84327cbe958b7bc68cdc7
yamaday0u <35hallelujah00@gmail.com>
2021-05-23 14:28:25 +0900

    Comment out in api/v1/rooms_controller

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -1,24 +1,24 @@
 class Api::V1::RoomsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   # protect_from_forgery with: :null_session
 
-  def index
-    mates = current_user.matchers
-    rooms = Room.where(talker_1: current_user.id)
+  # def index
+  #   mates = current_user.matchers
+  #   rooms = Room.where(talker_1: current_user.id)
     # data = []
     # data << mates
     # data << current_user
     # data << rooms
     # render json: data
-  end
+  # end
 
-  def create
-    room = Room.new(talker_1: current_user.name, talker_2: params[:user_id])
-      if room.save
-        redirect_to "/rooms/#{room.id}"
-      else
-        redirect_to "/rooms"
-      end
-  end
+  # def create
+  #   room = Room.new(talker_1: current_user.name, talker_2: params[:user_id])
+  #     if room.save
+  #       redirect_to "/rooms/#{room.id}"
+  #     else
+  #       redirect_to "/rooms"
+  #     end
+  # end
 
 end

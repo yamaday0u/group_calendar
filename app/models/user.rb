54b4d79ce5a8052ef6e9ece1823c4cbab4e84598
yamaday0u <35hallelujah00@gmail.com>
2021-06-18 23:02:15 +0900

    Make User model omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   
   include DeviseTokenAuth::Concerns::User
 

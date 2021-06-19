3dce11fa9666cf969bef72a2313722ad9ae4f1ea
yamaday0u <35hallelujah00@gmail.com>
2021-06-19 21:23:15 +0900

    Fix "from_omniauth" method

--- app/models/user.rb
+++ app/models/user.rb
@@ -78,7 +78,7 @@ class User < ApplicationRecord
 
     def self.from_omniauth(auth)
       sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
-      user = User.where(email: auth.infoemail).first_or_initialize(
+      user = User.where(email: auth.info.email).first_or_initialize(
         name: auth.info.name,
         email: auth.info.email
       )

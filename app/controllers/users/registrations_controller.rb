9678c49128e48e28f5536024b50d90e0db137771
yamaday0u <35hallelujah00@gmail.com>
2021-06-20 09:02:56 +0900

    Revert "Merge pull request #44 from yamaday0u/Implement-sns-credential-function"
    This reverts commit f82235a42646762fd9c57528f3c7f5f47aad9714, reversing
    changes made to 05bc7bc3ccc9480a17e7a22a4e9715d71970e875.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,14 +16,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  def create
-    if params[:sns_auth] == 'true'
-      pass = Devise.friendly_token
-      params[:user][:password] = pass
-      params[:user][:password_confirmation] = pass
-    end
-    super
-  end
+  # def create
+  #   super
+  # end
 
   # GET /resource/edit
   # def edit

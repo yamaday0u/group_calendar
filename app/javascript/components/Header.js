4596825e55b34d723b05dc3ba99f2bfd0f37cad9
yamaday0u <35hallelujah00@gmail.com>
2021-07-23 22:17:42 +0900

    Exchanged Header content from erb to js in top page

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -1,14 +1,30 @@
 import React from 'react'
 
-function Header() {
+function Header(props) {
   return (
     <>
       <header className="top__page__header">
         <div className="header__band">
-          <ul className="user__options">
-              <li>Hi, user</li>
-              <li>Log out</li>
-          </ul>
+          {
+            props.current_user?
+            (
+            <div>
+              <ul className="user__options">
+              <li><a href="/users/{props.current_user.id}" className="logged__in__user">Hi, {props.current_user.name}</a></li>
+              <li><a href="/users/sign_out" rel="nofollow"  data-method="delete" className="user__option">Log out</a></li>
+              </ul>
+            </div>
+            )
+            :
+            (
+            <div>
+              <ul className="user__options">
+              <li><a href="/users/sign_in" className="user__option">Log in</a></li>
+              <li><a href="/users/sign_up" className="user__option">Sign up</a></li>
+              </ul>
+            </div>
+            )
+          }
         </div>
       </header>
     </>

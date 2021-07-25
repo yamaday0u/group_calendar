c3b293ad30d093985ed9415e675c652e734d5936
yamaday0u <35hallelujah00@gmail.com>
2021-07-25 22:06:45 +0900

    Implement unit test for mate chat

--- spec/models/mate_chat_spec.rb
+++ spec/models/mate_chat_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe MateChat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @mate_chat = FactoryBot.build(:mate_chat)
+  end
+
+  describe '#create' do
+    context 'successfully' do
+      it 'is valid with text' do
+        expect(@mate_chat).to be_valid
+      end
+    end
+
+    context 'unsuccessfully' do
+      it 'is invalid without text' do
+        @mate_chat.text = ''
+        @mate_chat.valid?
+        expect(@mate_chat.errors.full_messages).to include("Text can't be blank")
+      end
+    end
+  end
 end

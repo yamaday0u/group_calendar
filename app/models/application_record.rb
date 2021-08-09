e55a75309a37cd20b27e2193d5726b05893a9633
yamaday0u <35hallelujah00@gmail.com>
2021-08-09 14:34:22 +0900

    Fix validation method "start_end_check"

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,7 +3,7 @@ class ApplicationRecord < ActiveRecord::Base
 
   # is used in models/calendar.rb & calendar_for_group.rb
   def start_end_check
-    if start_time.present?
+    if end_time.present?
       if start_time > end_time
         errors.add(:end_time, 'must set be late than Start time')
       end

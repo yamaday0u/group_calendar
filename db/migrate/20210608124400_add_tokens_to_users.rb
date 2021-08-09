83ad5f76739f5bd918d64c59f5faf44ad064fa7e
yamaday0u <35hallelujah00@gmail.com>
2021-08-09 21:05:35 +0900

    Update 20210608124400_add_tokens_to_users.rb

--- db/migrate/20210608124400_add_tokens_to_users.rb
+++ db/migrate/20210608124400_add_tokens_to_users.rb
@@ -1,6 +1,6 @@
 class AddTokensToUsers < ActiveRecord::Migration[6.0]
   def up
-    add_column :users, :provider, :string, null: false, default: 'email'
+    
     add_column :users, :uid, :string, null: false, default: ''
     add_column :users, :tokens, :text
     User.reset_column_information

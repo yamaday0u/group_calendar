719ea200a830e3638d588a69c5dab735bf258ed6
yamaday0u <35hallelujah00@gmail.com>
2021-08-09 14:12:33 +0900

    Change columns that content & end_time to null: true

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_133859) do
+ActiveRecord::Schema.define(version: 2021_08_09_045319) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -61,9 +61,9 @@ ActiveRecord::Schema.define(version: 2021_06_18_133859) do
 
   create_table "calendars", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
-    t.string "content", null: false
+    t.string "content"
     t.datetime "start_time", null: false
-    t.datetime "end_time", null: false
+    t.datetime "end_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false

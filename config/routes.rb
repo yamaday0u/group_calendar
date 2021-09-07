97d5431c1477b16c009151f82df40577d3340ead
yamaday0u <35hallelujah00@gmail.com>
2021-09-07 20:06:45 +0900

    Organize code for config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'home/new'
 
   resources :calendars
+
   resources :groups do
     resources :chats, only: %i[index create]
     resources :calendar_for_groups do
@@ -10,19 +11,24 @@ Rails.application.routes.draw do
     end
     resources :user_groups, only: %i[index create]
   end
+
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
+
   devise_scope :user do
     post 'users/guest_log_in', to: 'users/sessions#guest_log_in'
   end
+
   resources :users, only: %i[index show] do
     resources :relationships, only: %i[create destroy]
   end
+
   resources :rooms do
     resources :mate_chats, only: %i[index create]
   end
+  
   namespace :api do
     scope :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'

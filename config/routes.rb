171088fbc731b1e73fb607de233e86dadcbd7534
yamaday0u <35hallelujah00@gmail.com>
2021-09-07 20:04:32 +0900

    Delete route for api/v1/test

--- config/routes.rb
+++ config/routes.rb
@@ -27,8 +27,5 @@ Rails.application.routes.draw do
     scope :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
     end
-    namespace :v1 do
-      resources :test, only: %i[index]
-    end
   end
 end

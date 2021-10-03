12d7e3cdda1c052c9c9064787cb6433470a7341a
yamaday0u <35hallelujah00@gmail.com>
2021-10-03 22:41:14 +0900

    Update controllers for calendars & admin/calendars in order to CHECK_ADMIN

--- app/controllers/admin/calendars_controller.rb
+++ app/controllers/admin/calendars_controller.rb
@@ -1,13 +1,13 @@
 class Admin::CalendarsController < ApplicationController
-  before_action :check_admin
+  before_action :check_admin?
 
   def index
     @calendars = Calendar.where(user_id: current_user)
   end
 
   private
-  def check_admin
-    unless current_user.admin?
+  def check_admin?
+    unless current_user.admin
       redirect_to root_path
     end
   end

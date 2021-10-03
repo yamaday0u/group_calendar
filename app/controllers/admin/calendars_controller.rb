b71663dc8b261e579d843457e580c81f8483408d
yamaday0u <35hallelujah00@gmail.com>
2021-10-03 22:06:07 +0900

    Define controllers for admin/calendars

--- app/controllers/admin/calendars_controller.rb
+++ app/controllers/admin/calendars_controller.rb
@@ -1,2 +1,14 @@
 class Admin::CalendarsController < ApplicationController
+  before_action :check_admin
+
+  def index
+    @calendars = Calendar.where(user_id: current_user)
+  end
+
+  private
+  def check_admin
+    unless current_user.admin?
+      redirect_to root_path
+    end
+  end
 end

15694c1a9c12a04666f5a90bfe77b26960402ab5
yamaday0u <35hallelujah00@gmail.com>
2021-10-03 21:27:02 +0900

    Create 'admin' column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_122807) do
+ActiveRecord::Schema.define(version: 2021_10_03_122402) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -162,6 +162,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_122807) do
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
     t.text "tokens"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true

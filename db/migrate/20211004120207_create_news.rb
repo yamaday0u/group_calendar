ec59e13c6709dc09fa0fc1cb4a521a007284ba8e
yamaday0u <35hallelujah00@gmail.com>
2021-10-04 21:04:49 +0900

    Create news table

--- db/migrate/20211004120207_create_news.rb
+++ db/migrate/20211004120207_create_news.rb
@@ -1,6 +1,8 @@
 class CreateNews < ActiveRecord::Migration[6.0]
   def change
     create_table :news do |t|
+      t.string :title, null: false
+      t.string :content, null: false
 
       t.timestamps
     end
